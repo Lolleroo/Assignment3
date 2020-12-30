836512b48939f6aaa371073378135228a253c2d2
Lolleroo <73130551+Lolleroo@users.noreply.github.com>
2020-12-30 13:56:12 +0200

    Add files via upload

---
+++ Assignment3.java
@@ -0,0 +1,11 @@
+public class Assignment3 {
+    public static void main(String[] args) {
+        int c = 0;
+        while (c <1001) {
+            if (c % 5 == 0) {
+                System.out.print(c + "\n");
+            }
+            c++;
+        }
+    }
+}
